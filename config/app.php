0cf7e4c572207781656319ff545493b91cf38f3e
JayjayCob <Jaycob@greenx.network>
2018-08-02 00:30:26 +0700

    v0.2: added 2FA authentication
    User needs to install Google Authenticator on mobile to do a 2FA auth before register/login
    
    Install:
    - composer install
    - php artisan migrate

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        PragmaRX\Google2FALaravel\ServiceProvider::class,
     ],
 
     /*
@@ -208,6 +209,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Google2FA' => PragmaRX\Google2FALaravel\Facade::class,
 
     ],
 
---
+++ resources/views/auth/google2fa.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Set up Google Authenticator</div>
+
+                <div class="panel-body" style="text-align: center;">
+                    Set up your two factor authentication by scanning the barcode below. 
+                    Alternatively, you can use the code {{ $secret }}
+                    <div>
+                        <img src="{{ $QR_Image }}">
+                    </div>
+                    You must set up your Google Authenticator app before continuing. You will be unable to login otherwise
+                    <div>
+                        <a href="/complete-registration"><button class="btn-primary">Complete Registration</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
